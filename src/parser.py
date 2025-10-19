92cc0faa8f146fa3d1aea9f7f3f6b1bf8b14f3eb
Leanid Barok <tratos149@gmail.com>
2025-10-19 11:52:41 +0300

    Add basic text to textnode parsing

--- src/parser.py
+++ src/parser.py
@@ -4,6 +4,16 @@ from textnode import TextNode, TextType
 image_regex = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
 link_regex = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
 
+def text_to_textnodes(text):
+    node = TextNode(text, TextType.TEXT)
+    current_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+    current_nodes = split_nodes_delimiter(current_nodes, '_', TextType.ITALIC)
+    current_nodes = split_nodes_delimiter(current_nodes, '`', TextType.CODE)
+    current_nodes = split_nodes_image(current_nodes)
+    current_nodes = split_nodes_link(current_nodes)
+
+    return current_nodes
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     
@@ -25,6 +35,52 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
     return new_nodes
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        images = extract_markdown_images(node.text)
+        start = 0
+        for image in images:
+            image_substring = f"![{image[0]}]({image[1]})"
+            position = node.text.find(image_substring)
+
+            if position != -1:
+                new_nodes.append(TextNode(node.text[start:position], TextType.TEXT))
+                new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+                start = position + len(image_substring)
+        
+        if start < len(node.text):
+            new_nodes.append(TextNode(node.text[start:], TextType.TEXT))
+        
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        links = extract_markdown_links(node.text)
+        start = 0
+        for link in links:
+            link_substring = f"[{link[0]}]({link[1]})"
+            position = node.text.find(link_substring)
+            if position != -1:
+                new_nodes.append(TextNode(node.text[start:position], TextType.TEXT))
+                new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+                start = position + len(link_substring)
+
+        if start < len(node.text):
+            new_nodes.append(TextNode(node.text[start:], TextType.TEXT))
+
+    return new_nodes
+
 def extract_markdown_images(text):
     matches = re.findall(image_regex, text)
     return matches
@@ -32,3 +88,5 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(link_regex, text)
     return matches
+
+
